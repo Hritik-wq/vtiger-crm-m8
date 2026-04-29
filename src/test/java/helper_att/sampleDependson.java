91c736377f3302718c13c991642f32a7bf4634da
Hritik-wq <hitikpandita402@gmail.com>
2026-04-29 08:19:23 +0530

    Helper Att Update

--- src/test/java/helper_att/sampleDependson.java
+++ src/test/java/helper_att/sampleDependson.java
@@ -1,6 +1,7 @@
 package helper_att;
 
 import org.testng.annotations.Test;
+import org.junit.Assert;
 import org.testng.Reporter;
 
 public class sampleDependson {
@@ -12,6 +13,8 @@ public class sampleDependson {
 
     @Test(dependsOnMethods = "createCity")
     public void modifyCity() {
+    	// To Check if Skips
+    	Assert.assertTrue(false);
         Reporter.log("Noida=>SmartNoida", true);
     }
 
@@ -19,4 +22,11 @@ public class sampleDependson {
     public void deleteCity() {
         Reporter.log("SmartNoida deleted", true);
     }
+    
+//    @Test()
+//    public void deleteCity() {
+//        Reporter.log("Noida=>SmartNoida", true);
+//        Reporter.log("SmartNoida deleted", true);
+//    }
+    
 }
--- src/test/java/helper_att/samplePriority.java
+++ src/test/java/helper_att/samplePriority.java
@@ -1,22 +1,26 @@
 package helper_att;
 
 import org.testng.annotations.Test;
+import org.junit.Assert;
 import org.testng.Reporter;
 
 public class samplePriority {
 
     @Test(priority = -1)
     public void createCity() {
+    	Assert.assertTrue(true);
         Reporter.log("created Noida", true);
     }
 
     @Test(priority = 0)
     public void modifyCity() {
+    	Assert.assertTrue(false);
         Reporter.log("Noida=>SmartNoida", true);
     }
 
     @Test(priority = 1)
     public void deleteCity() {
+    	Assert.assertTrue(true);
         Reporter.log("SmartNoida deleted", true);
     }
 }
